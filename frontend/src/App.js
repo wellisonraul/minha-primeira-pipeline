6038e22585c6682bad16319b5481cf49f93b5566
Wellison Santos <wellisonraulm@gmail.com>
2025-11-29 13:30:44 +0000

    :feat: new data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const URL = "http://backend:3000";
+const URL = "http://54.234.28.1:3000";
 
 
 function App() {
